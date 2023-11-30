f4168251a6461560003e410099af677738684b4f
Heartheker656 <152202029+Heartheker656@users.noreply.github.com>
2023-11-30 22:16:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 
 BOT_TOKEN = getenv("BOT_TOKEN")
-OWNER_ID = int(getenv("6852847382"))
-
+OWNER_ID = int(getenv(OWNER_ID, "6852847382"))
 MONGO_DB_URI = getenv("MONGO_DB_URI")
 MUST_JOIN = getenv("MUST_JOIN", None)
